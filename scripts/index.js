4e5217c702c6cfb97517b812cf32fdbdbe328cdf
nhat31032k <72483745+nhat31032k@users.noreply.github.com>
2021-12-08 14:39:30 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-    birthdate: 'Aug 22, 2021',
+    birthdate: 'Dec 9, 2021',
     name: ''
 };
 
